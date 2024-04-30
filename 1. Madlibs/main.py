c53381262ebe7882cce1dcab2e8733478ec86c44
souro303 <sourovedatta5@gmail.com>
2024-04-30 13:07:44 +0600

    initial commit

--- 1. Madlibs/main.py
+++ 1. Madlibs/main.py
@@ -1,5 +1,14 @@
-youtube = 'adadfa'
+# youtube = 'adadfa'
 
-print("Subscribt to " + youtube)
-print("Subscribt to {}".format(youtube))
-print(f"Subscribt to {youtube}")
+# print("Subscribt to " + youtube)
+# print("Subscribt to {}".format(youtube))
+# print(f"Subscribt to {youtube}")
+
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
+    I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
